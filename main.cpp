f5c01790966d7d2e1be2791d64bc3368c51c764e
Vincent Villani <villanivincent@gmail.com>
2016-01-07 19:32:53 +1100

    Messing around with threads

--- main.cpp
+++ main.cpp
@@ -9,11 +9,20 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <thread>
+#include <chrono>
+
 
 #include "ChatServer.h"
 
 int main()
 {
+	std::chrono::duration<int> tenSeconds = std::chrono::duration<int>(10);
+
+	printf("Sleeping\n");
+	std::this_thread::sleep_for(tenSeconds);
+	printf("Awake\n");
+
 	ChatServer server;
 
 	server.update();
